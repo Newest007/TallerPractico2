3189af7d381919f3f9fdaf513ad24fd70894ee79
luispablovelasco <pablo.velascoflores@gmail.com>
2021-10-07 17:12:33 -0600

    Creacion de clases: Usuario y Encriptado

---
+++ ventasExpress/UsuarioEncriptado.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ventasExpress
+{
+    class UsuarioEncriptado //Esta clase guardará los datos ya encriptados
+    {
+        protected string usuarioencript; //Aqui se almacenarán los usuarios ya encriptados
+        protected string contraencript;  //Aquí se almacenarán las contraseñas ya encriptadas
+
+        public string Usuarioencript
+        {
+            get { return usuarioencript; }
+            set { usuarioencript = value; }
+        }
+
+        public string Contraencript
+        {
+            get { return contraencript; }
+            set { contraencript = value; }
+        }
+    }
+}
